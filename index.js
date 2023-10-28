e3396be64307fbbadd5fd88ac568f506700b7e73
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-10-27 20:59:00 -0500

    updated index,js and generatMarkdown files

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-
 const generateMarkdown = require("./utils/generateMarkdown");
+const path = require("path");
 // TODO: Create an array of questions for user input
 const questions = [
     {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns the license section of README
+// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if(license !== "none") {
@@ -6,6 +6,15 @@ function renderLicenseSection(license) {
   }
   return "";
 }
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
